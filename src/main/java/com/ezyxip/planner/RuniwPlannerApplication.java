4e72ad8b044e1cc27ba8255e452dc79c5fe5b258
Antey <anzej2003@gmail.com>
2023-05-05 23:32:50 +0600

    Инициирующий коммит
    Первый коммит в проект, который представляет собой реализацию модуля планирования событий системы управления складом

---
+++ src/main/java/com/ezyxip/planner/RuniwPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.ezyxip.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuniwPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuniwPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezyxip/planner/RuniwPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezyxip.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuniwPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
